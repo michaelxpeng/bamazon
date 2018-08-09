8f96db744d94866c151386253f3ae6fa9e0afb8c
mpeng <michaelxpeng@gmail.com>
2018-08-09 00:13:37 -0400

    purchase function added

--- bamazon.sql
+++ bamazon.sql
@@ -13,4 +13,16 @@ CREATE TABLE products(
     PRIMARY KEY (item_id)
 );
 
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("5-piece tenders combo", "Popeyes", 9.29, 10),
+("Double-Double burger", "In-N-Out", 3.45, 15),
+("8-piece family meal", "KFC", 14.99, 12),
+("Roasted Turkey & Avocado BLT", "Panera Bread", 8.99, 10),
+("A dozen donuts", "Dunkin' Donuts", 9.99, 20),
+("Build-your-own bowl", "Chipotle", 7.50, 15),
+("Sweet Onion Chicken Teriyaki sandwich", "Subway", 7.75, 18),
+("Bacon Cheeseburger", "Five Guys", 8.69, 15),
+("20-piece Chicken McNuggets", "McDonald's", 5, 10),
+("Jumbo Jack Cheeseburger", "Jack in the Box", 5.99, 12);
+
 SELECT * FROM products;
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -11,6 +11,50 @@ var connection = mysql.createConnection({
 
 connection.connect(function (err) {
   if (err) throw err;
-  console.log("connected as id " + connection.threadId);
-  connection.end();
+  initialize();
 });
+
+function initialize() {
+  var mealArr = [];
+  connection.query("SELECT * FROM products", function (err, res) {
+  if (err) throw err
+  for (var i = 0; i < res.length; i++) {
+    mealArr.push(res[i].item_id.toString());
+    console.log("Product ID: " + res[i].item_id + "\nProduct name: " + res[i].product_name + "\nRestaurant: " + res[i].department_name + "\nPrice: $" + res[i].price + "\nStock quantity: " + res[i].stock_quantity + "\n--------------------");
+  };
+
+  inquirer.prompt([
+    {
+      type: 'list',
+      name: 'choice',
+      message: 'Which meal would you like?',
+      choices: mealArr
+    },
+    {
+      type: 'input',
+      name: 'quantity',
+      message: 'How many orders would you like?'
+    }
+  ]).then(function (answers) {
+    var meal = answers.choice - 1;
+    var quantity = answers.quantity;
+
+    if (quantity <= res[meal].stock_quantity) {
+      connection.query("UPDATE products SET ? WHERE ?"),
+      [
+        {
+          stock_quantity: (res[meal].stock_quantity - quantity)
+        },
+        {
+          item_id: meal
+        }
+      ],function(err, result) {
+        if(err) throw err;
+        console.log("Coming right up!");
+        };
+    } else{
+      console.log("Sorry, we don't have enough left.");
+    };
+  });
+  });
+};
