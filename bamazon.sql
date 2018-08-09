79f43247bff3aa564e1c01129e2aa1352eef7f67
mpeng <michaelxpeng@gmail.com>
2018-08-09 11:24:08 -0400

    fixed purchase function/added update function

--- bamazon.sql
+++ bamazon.sql
@@ -17,11 +17,11 @@ INSERT INTO products (product_name, department_name, price, stock_quantity)
 VALUES ("5-piece tenders combo", "Popeyes", 9.29, 10),
 ("Double-Double burger", "In-N-Out", 3.45, 15),
 ("8-piece family meal", "KFC", 14.99, 12),
-("Roasted Turkey & Avocado BLT", "Panera Bread", 8.99, 10),
-("A dozen donuts", "Dunkin' Donuts", 9.99, 20),
-("Build-your-own bowl", "Chipotle", 7.50, 15),
-("Sweet Onion Chicken Teriyaki sandwich", "Subway", 7.75, 18),
-("Bacon Cheeseburger", "Five Guys", 8.69, 15),
+("roasted turkey & avocado BLT", "Panera Bread", 8.99, 10),
+("a dozen donuts", "Dunkin' Donuts", 9.99, 20),
+("build-your-own bowl", "Chipotle", 7.50, 15),
+("sweet onion chicken Teriyaki sandwich", "Subway", 7.75, 18),
+("bacon cheeseburger", "Five Guys", 8.69, 15),
 ("20-piece Chicken McNuggets", "McDonald's", 5, 10),
 ("Jumbo Jack Cheeseburger", "Jack in the Box", 5.99, 12);
 
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -20,7 +20,13 @@ function initialize() {
   if (err) throw err
   for (var i = 0; i < res.length; i++) {
     mealArr.push(res[i].item_id.toString());
-    console.log("Product ID: " + res[i].item_id + "\nProduct name: " + res[i].product_name + "\nRestaurant: " + res[i].department_name + "\nPrice: $" + res[i].price + "\nStock quantity: " + res[i].stock_quantity + "\n--------------------");
+    console.log("Product ID: " + res[i].item_id + 
+    "\nProduct name: " + res[i].product_name + 
+    "\nRestaurant: " + res[i].department_name + 
+    "\nPrice: $" + res[i].price + 
+    "\nStock quantity: " + res[i].stock_quantity + 
+    "\n--------------------");
+    
   };
 
   inquirer.prompt([
@@ -36,24 +42,28 @@ function initialize() {
       message: 'How many orders would you like?'
     }
   ]).then(function (answers) {
-    var meal = answers.choice - 1;
+    var meal = answers.choice;
     var quantity = answers.quantity;
+    console.log(meal, res[meal-1].product_name);
 
-    if (quantity <= res[meal].stock_quantity) {
-      connection.query("UPDATE products SET ? WHERE ?"),
+    if (quantity <= res[meal-1].stock_quantity) {
+      connection.query("UPDATE products SET ? WHERE ?",
       [
         {
-          stock_quantity: (res[meal].stock_quantity - quantity)
+          stock_quantity: (res[meal-1].stock_quantity - quantity)
         },
         {
           item_id: meal
         }
-      ],function(err, result) {
+      ],
+      function(err) {
         if(err) throw err;
-        console.log("Coming right up!");
-        };
-    } else{
+        console.log(quantity + " orders of " + res[meal-1].product_name + " coming right up!");
+        connection.end();
+        });
+    } else {
       console.log("Sorry, we don't have enough left.");
+      connection.end();
     };
   });
   });
